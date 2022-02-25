fc8b187636ce729a7b963727a4fe8106cbee85d1
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-25 17:06:29 +0100

    created name, mail and age

---
+++ snack1.php
@@ -0,0 +1,26 @@
+<?php
+$name = $_GET ? $_GET['name'] : '';
+echo $name;
+
+
+$mail = $_GET ? $_GET['mail'] : '';
+echo $mail;
+
+$age = $_GET ? $_GET['age'] : '';
+echo $age; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snak1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
