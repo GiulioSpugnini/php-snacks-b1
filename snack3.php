c6b84191bac4b32bf961b94bbad202b7fea9e9bd
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-02-28 00:53:47 +0100

    snack 2 completed

---
+++ snack3.php
@@ -0,0 +1,24 @@
+<!-- Snack 3
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta. -->
+
+<?php 
+    $array= [];
+    while(count($array)<15){
+        $random_number= rand(0,100);
+        if(in_array($random_number,$array)) array_push($array,$random_number);
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack3</title>
+</head>
+<body>
+    <h2><?php print_r($array)?></h2>
+</body>
+</html>
